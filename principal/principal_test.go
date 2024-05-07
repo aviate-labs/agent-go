0f28975fb5f4d7267b8e48135aae4ccf0a4cca8a
Quint Daenen <quint@daenen.email>
2024-05-07 11:10:58 +0200

    Add principal types.

--- principal/principal_test.go
+++ principal/principal_test.go
@@ -3,6 +3,8 @@ package principal_test
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/aviate-labs/agent-go/ic"
+	"testing"
 
 	"github.com/aviate-labs/agent-go/principal"
 )
@@ -16,8 +18,23 @@ func ExampleDecode() {
 
 func ExamplePrincipal() {
 	raw, _ := hex.DecodeString("abcd01")
-	p := principal.Principal{raw}
+	p := principal.Principal{Raw: raw}
 	fmt.Println(p.Encode())
 	// Output:
 	// em77e-bvlzu-aq
+}
+
+func TestPrincipal(t *testing.T) {
+	if !ic.LEDGER_PRINCIPAL.IsOpaque() {
+		t.Fatal("expected opaque principal")
+	}
+	if !principal.MustDecode("g27xm-fnyhk-uu73a-njpqd-hec7y-syhwe-bd45b-qm6yc-xikg5-cylqt-iae").IsSelfAuthenticating() {
+		t.Fatal("expected derived principal")
+	}
+	if !principal.AnonymousID.IsAnonymous() {
+		t.Fatal("expected anonymous principal")
+	}
+	if !(principal.Principal{Raw: append([]byte("random"), 0x7f)}).IsReserved() {
+		t.Fatal("expected reserved principal")
+	}
 }
