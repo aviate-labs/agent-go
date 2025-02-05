27664d811ecf110d4ebfed019105b478108f6764
Quint Daenen <quint@daenen.email>
2025-02-05 19:56:51 +0100

    Support call v3.

--- clients/registry/client_test.go
+++ clients/registry/client_test.go
@@ -1,9 +1,11 @@
 package registry_test
 
 import (
-	"github.com/aviate-labs/agent-go/clients/registry"
 	"os"
 	"testing"
+
+	"github.com/aviate-labs/agent-go/clients/registry"
+	"github.com/aviate-labs/agent-go/principal"
 )
 
 func TestClient_GetNodeListSince(t *testing.T) {
@@ -21,6 +23,23 @@ func TestClient_GetNodeListSince(t *testing.T) {
 
 	if _, err := c.GetNodeListSince(latestVersion - 100); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestClient_GetNNSSubnetID(t *testing.T) {
+	checkEnabled(t)
+
+	c, err := registry.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id, err := c.GetNNSSubnetID()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !id.Equal(principal.MustDecode("tdb26-jop6k-aogll-7ltgs-eruif-6kk7m-qpktf-gdiqx-mxtrf-vb5e6-eqe")) {
+		t.Error(id)
 	}
 }
 
